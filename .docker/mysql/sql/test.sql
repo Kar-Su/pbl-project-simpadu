759a8e4e0b2cc67c910ac166a042fff0c70688bb
Kar <rumahhelmi@gmail.com>
2026-05-05 19:27:09 +0800

    fix(sql): Collate error
    Signed-off-by: Kar <rumahhelmi@gmail.com>

--- .docker/mysql/sql/test.sql
+++ .docker/mysql/sql/test.sql
@@ -1,4 +1,7 @@
-CREATE DATABASE if not exists test;
+DROP DATABASE IF EXISTS test;
+CREATE DATABASE test
+    DEFAULT CHARACTER SET utf8mb4
+    DEFAULT COLLATE utf8mb4_bin;
 
 use test
 
@@ -7,7 +10,7 @@ CREATE TABLE if not exists roles (
     name VARCHAR(255) NOT NULL,
 
     UNIQUE INDEX idx_roles_name (name)
-);
+) engine=InnoDB;
 
 CREATE TABLE if not exists users (
     id char(36) PRIMARY KEY,
@@ -23,7 +26,7 @@ CREATE TABLE if not exists users (
     UNIQUE INDEX idx_users_email (email),
     UNIQUE INDEX idx_users_detail (detail_id),
     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id)
-);
+) engine=InnoDB;
 
 CREATE TABLE IF NOT EXISTS refresh_tokens (
     id char(36) PRIMARY KEY,
@@ -36,14 +39,14 @@ CREATE TABLE IF NOT EXISTS refresh_tokens (
     UNIQUE INDEX idx_token (token),
     INDEX idx_token_user (user_id),
     CONSTRAINT fk_token_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-);
+) engine=InnoDB;
 
 CREATE TABLE IF NOT EXISTS jurusan(
     id int PRIMARY KEY AUTO_INCREMENT,
     nama VARCHAR(255) NOT NULL,
 
     UNIQUE INDEX idx_jurusan_nama (nama)
-);
+) engine=InnoDB;
 
 CREATE TABLE IF NOT EXISTS prodi(
     id int PRIMARY KEY AUTO_INCREMENT,
@@ -53,13 +56,13 @@ CREATE TABLE IF NOT EXISTS prodi(
     UNIQUE INDEX idx_prodi_nama (nama),
     INDEX idx_prodi_jurusan (jurusan_id),
     CONSTRAINT fk_prodi_jurusan FOREIGN KEY (jurusan_id) REFERENCES jurusan(id) ON DELETE CASCADE
-);
+) engine=InnoDB;
 
 CREATE TABLE IF NOT EXISTS mata_kuliah(
     id char(36) PRIMARY KEY,
     nama_mk VARCHAR(255) NOT NULL,
     sks int NOT NULL
-);
+) engine=InnoDB;
 
 CREATE TABLE IF NOT EXISTS kurikulum(
     id char(36) PRIMARY KEY,
@@ -67,7 +70,7 @@ CREATE TABLE IF NOT EXISTS kurikulum(
     prodi_id int NOT NULL,
 
     CONSTRAINT fk_kurikulum_prodi FOREIGN KEY (prodi_id) REFERENCES prodi(id) ON DELETE CASCADE
-);
+) engine=InnoDB;
 
 CREATE TABLE IF NOT EXISTS kurikulum_mk(
     kurikulum_id char(36) NOT NULL,
@@ -76,7 +79,7 @@ CREATE TABLE IF NOT EXISTS kurikulum_mk(
     PRIMARY KEY (kurikulum_id, mata_kuliah_id),
     CONSTRAINT fk_kurikulum_mk_kurikulum FOREIGN KEY (kurikulum_id) REFERENCES kurikulum(id) ON DELETE CASCADE,
     CONSTRAINT fk_kurikulum_mk_mata_kuliah FOREIGN KEY (mata_kuliah_id) REFERENCES mata_kuliah(id) ON DELETE CASCADE
-);
+) engine=InnoDB;
 
 CREATE TABLE IF NOT EXISTS tahun_akademik(
     id int PRIMARY KEY,
@@ -85,7 +88,7 @@ CREATE TABLE IF NOT EXISTS tahun_akademik(
     tahun_akhir date NOT NULL,
 
     UNIQUE INDEX idx_tahun_akademik_tahun (tahun_awal, tahun_akhir)
-);
+) engine=InnoDB;
 
 CREATE TABLE IF NOT EXISTS kelas(
     id char(36) PRIMARY KEY,
@@ -97,7 +100,7 @@ CREATE TABLE IF NOT EXISTS kelas(
     CONSTRAINT fk_kelas_tahun_akademik FOREIGN KEY (tahun_akademik_id) REFERENCES tahun_akademik(id) ON DELETE CASCADE,
     CONSTRAINT fk_kelas_kurikulum FOREIGN KEY (kurikulum_id) REFERENCES kurikulum(id) ON DELETE CASCADE,
     CONSTRAINT fk_kelas_prodi FOREIGN KEY (prodi_id) REFERENCES prodi(id) ON DELETE CASCADE
-);
+) engine=InnoDB;
 
 CREATE TABLE IF NOT EXISTS kelas_mahasiswa(
     kelas_id char(36) NOT NULL,
@@ -106,4 +109,4 @@ CREATE TABLE IF NOT EXISTS kelas_mahasiswa(
     PRIMARY KEY (kelas_id, mahasiswa_id),
     CONSTRAINT fk_kelas_mahasiswa_kelas FOREIGN KEY (kelas_id) REFERENCES kelas(id) ON DELETE CASCADE,
     CONSTRAINT fk_kelas_mahasiswa_mahasiswa FOREIGN KEY (mahasiswa_id) REFERENCES users(detail_id) ON UPDATE CASCADE
-);
+) engine=InnoDB;
